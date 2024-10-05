a923cc34e267185e6e1cbac58dcc883920b86b3c
Rahul Khambra <112678535+R001K@users.noreply.github.com>
2024-10-05 18:16:45 +0000

    dataTypes

---
+++ .devcontainer/Basic1/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all Js code as newer version
+
+// alert("hello") // we are not using JS, not broswer
+// alert(3 + 3)
+
+console.log( 3 + 3); // code readable should be High 
+
+let name = "rahul"
+let age = 23
+let isLoggedIn = true
+let state = null 
+
+
+// DataTypes
+// number =  2 to power 53
+// bigInt = 
+// string = ""
+// boolean = true, false
+// null =  standalone value
+// undefined => Value is not assigned yet
+// symbol => unique 
+
+//object  => 
+
+ console.log(typeof undefined);      // output = undefined
+ console.log(typeof null);          // output = object
+
+    
