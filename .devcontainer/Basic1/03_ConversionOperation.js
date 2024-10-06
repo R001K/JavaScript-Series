ac9f36f4cd2bd05bd5502d3f0667e405248e8ea0
Rahul Khambra <112678535+R001K@users.noreply.github.com>
2024-10-06 10:30:09 +0000

    conversion of dataTypes

---
+++ .devcontainer/Basic1/03_ConversionOperation.js
@@ -0,0 +1,29 @@
+let scorce = "33abc"
+
+console.log(typeof scorce);
+console.log(typeof (scorce));
+
+let valueInNumber = Number(scorce)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//  33 => number
+// "33" =>String
+// "33abc" => NaN
+
+let isLoggedIn = 0
+
+let boolenISLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boolenISLoggedIn);
+console.log(boolenISLoggedIn);
+
+// Boolean true => 1 , false => 0
+// "" => false
+// "rahul" => true
+
+let someNumber = 67
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
